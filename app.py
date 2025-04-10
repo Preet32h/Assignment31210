6a957aa3dcdd1be6fa1aabc6a7bdd3a03d951abb
Preet32h <97974547+Preet32h@users.noreply.github.com>
2025-04-10 16:18:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-o	from flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_cloud():
-  return 'Hello from Singh ECS container'
+def hello():
+    return "Hello from Singh ECS container "
 
-app.run(host='0.0.0.0')
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
