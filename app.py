c47951468400d9e6a1a24928668c400edfe545d9
Preet32h <97974547+Preet32h@users.noreply.github.com>
2025-04-14 16:05:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Singh ECS container "
+    return "Hello, Wild Rydes!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
